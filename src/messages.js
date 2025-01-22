c8e2588b7d9c24ef629c9db49fe032072ca09435
WILLE FERREIRA <wille26ferreira@gmail.com>
2025-01-22 17:55:07 -0400

    adicionado atualizção de img a cada evento, arrumado o erro ao fazer logout

--- src/messages.js
+++ src/messages.js
@@ -1,6 +1,7 @@
 // messages.js
 
 import {inBusinessHours} from './Functions/inBusinessHours.js'
+
 export async function handleMessages(client) {
   // Quando o WhatsApp Web estiver pronto para ser usado
   client.on("ready", () => {
@@ -9,7 +10,11 @@ export async function handleMessages(client) {
   const hours = inBusinessHours();
   // Receber mensagens
   client.on("message", (message) => {
-    console.log(hours, 'textes')
+    if (chatId.endsWith("@g.us")) {
+      console.log(`Mensagem não enviada. O chat ${chatId} é um grupo.`);
+      return;
+    }
+    
     if (!hours) {
       // Mensagem fora do horário de expediente
       message.reply(`Olá,\n\nInfelizmente o atendimento de hoje já encerrou. Estamos fora da loja neste momento e não poderemos responder imediatamente. Caso queira deixar sua dúvida, lhe retornaremos assim que possível. \n\n🕐Horário de expediente de segunda a sexta das 9h até as 19h, e aos sábados das 9h até as 17h.\n\nTenha uma ótima noite🌙!\n\nAtenciosamente`
--- src/qrCode.js
+++ src/qrCode.js
@@ -1,13 +1,47 @@
+import fs from "fs";
 import pkg from "whatsapp-web.js";
 import { fileURLToPath } from "url";
 import path from "path";
 import qrCode from "qrcode"; // Importação correta do pacote qrcode
 import qrCodeTerminal from "qrcode-terminal";
-const { Client, LocalAuth } = pkg;
+import { exec } from "child_process";
 
+const { Client, LocalAuth } = pkg;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const userId = '12456'
 export function initializeWhatsApp(io) {
+  const authStrategy = new LocalAuth({
+    clientId: userId, 
+});
   const client = new Client({
-    authStrategy: new LocalAuth(), // ou qualquer outro método de autenticação
+    authTimeoutMs: 20000,
+    takeoverOnConflict: true,
+    authStrategy,
+    restartOnAuthFail: true,
+    puppeteer: {
+      ignoreDefaultArgs: ['--enable-automation', '--disable-dev-shm-usage'],
+      headless: true,
+      args: ['--no-sandbox', '--disable-gpu-driver-bug-workarounds', '--disable-setuid-sandbox', '--unhandled-rejections=strict','--disable-dev-shm-usage','--disable-accelerated-2d-canvas','--no-first-run','--no-zygote', '--disable-gpu',  '--log-level=3',  '--no-default-browser-check',  '--disable-site-isolation-trials',  '--no-experiments',  '--ignore-gpu-blacklist',  '--ignore-certificate-errors',  '--ignore-certificate-errors-spki-list',  '--disable-extensions',  '--disable-default-apps',  '--enable-features=NetworkService',  '--disable-webgl',  '--disable-threaded-animation',  '--disable-threaded-scrolling',  '--disable-in-process-stack-traces',  '--disable-histogram-customizer',  '--disable-gl-extensions',  '--disable-composited-antialiasing',  '--disable-canvas-aa',  '--disable-3d-apis',  '--disable-accelerated-jpeg-decoding',  '--disable-accelerated-mjpeg-decode',  '--disable-app-list-dismiss-on-blur',  '--disable-accelerated-video-decode']
+    },
+  });
+
+  client.on("disconnected", async (reason) => {
+    await client.destroy();
+    if (reason == "NAVIGATION" || reason == "LOGOUT") {
+      const folderPath = path.join(
+        __dirname,
+        `../../../.wwebjs_auth/session-${client.authStrategy.clientId}`
+      );
+      fs.rm(folderPath, { recursive: true, force: true }, (err) => {
+        if (err) {
+          console.log(`Error deleting folder: ${err.message}`);
+        } else {
+          console.log("Folder deleted successfully");
+        }
+      });
+    }
+    client.initialize();
   });
 
   // Quando o QR Code for gerado
@@ -29,7 +63,7 @@ export function initializeWhatsApp(io) {
           return;
         }
         console.log("QR Code salvo como imagem em:", qrPath);
-        io.emit("qr", "/qr.png"); // Envia o caminho da imagem para o frontend
+        io.emit("qr", "/qr.png");; // Envia o caminho da imagem para o frontend
       });
 
       // Exibir o QR Code no terminal para debug
@@ -39,7 +73,10 @@ export function initializeWhatsApp(io) {
     }
   });
 
-  // Inicializa o cliente WhatsApp
+  client.on("ready", () => {
+    console.log("Cliente do WhatsApp está pronto!");
+  });
+
   client.initialize();
 
   return client;
